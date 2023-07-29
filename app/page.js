f8fc64c6e91f8f8cbe2b9fa6f45c2ae8cbb12b97
Matt Dean <mdean400@gmail.com>
2023-07-28 20:15:55 -0700

    nodejs runtime

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import styles from './page.module.css'
 import { unstable_cache } from 'next/cache'
 
-export const runtime = 'edge'
 export const fetchCache = 'force-no-store'
 export const dynamic = 'force-dynamic'
 
